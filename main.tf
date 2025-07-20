d78ae3c881c076c3a81c30970dce6e57dba63560
devopsprashu <prashansachincholkar@gmail.com>
2025-07-20 19:37:17 +0530

    new rg added

--- main.tf
+++ main.tf
@@ -15,3 +15,8 @@ resource "azurerm_resource_group" "example" {
   name     = "example"
   location = "West Europe"
 }
+
+resource "azurerm_resource_group" "demo" {
+  name     = "demo-rg"
+  location = "West Europe"
+}
