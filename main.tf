f4ccb34115c5a314814549ce06703d63978174f1
devopsprashu <prashansachincholkar@gmail.com>
2025-07-20 19:57:48 +0530

    dhondhu rg added

--- main.tf
+++ main.tf
@@ -25,3 +25,8 @@ resource "azurerm_resource_group" "test" {
   name     = "test-rg"
   location = "West Europe"
 }
+
+resource "azurerm_resource_group" "dhondhu" {
+  name     = "dhondhu-rg"
+  location = "Central India"
+}
